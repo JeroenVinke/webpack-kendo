47dccc9e73b19b7aa743f387a384992597f6ae43
Jeroen <Jeroenvinke17@gmail.com>
2016-07-21 14:28:45 +0200

    no message

---
+++ src/welcome.js
@@ -0,0 +1,31 @@
+export class Welcome {
+  constructor() {
+    this.datasource = {
+      type: 'odata',
+      transport: {
+        read: '//demos.telerik.com/kendo-ui/service/Northwind.svc/Orders'
+      },
+      schema: {
+        model: {
+          fields: {
+            OrderID: { type: 'number' },
+            Freight: { type: 'number' },
+            ShipName: { type: 'string' },
+            OrderDate: { type: 'date' },
+            ShipCity: { type: 'string' }
+          }
+        }
+      },
+      pageSize: 9,
+      serverPaging: true,
+      serverFiltering: true,
+      serverSorting: true
+    };
+  }
+}
+
+export class UpperValueConverter {
+  toView(value) {
+    return value && value.toUpperCase();
+  }
+}
